a3b50ead6ddf616aef86c86d411cba2e34f9a6bf
jakeggreen <80248380+jakeggreen@users.noreply.github.com>
2021-04-06 15:29:34 +0100

    Update events.py

--- cogs/events.py
+++ cogs/events.py
@@ -6,6 +6,7 @@ import discord
 class Events(Cog):
 	def __init__(self, bot):
 		self.bot = bot
+		self.msg_delete_time = 60
 
 	@Cog.listener()
 	async def on_message(self, message):
@@ -20,16 +21,24 @@ class Events(Cog):
 
 	@Cog.listener()
 	async def on_member_update(self, before, after):
-		if before.activity == None:
+		print(before.activity, before.status, after.activity, after.status)
+		if before.status != 'online':
 			channel = before.guild.system_channel
-			await channel.send(f'{before.display_name} is now {after.status}!')
-		else:
+			await channel.send(f'{after.display_name} is now {after.status}!', delete_after=self.msg_delete_time)
+
+		if before.activity == None and after.activity != None:
 			channel = before.guild.system_channel
-			party_min = after.activity.party.get('size')[0]
-			party_max = after.activity.party.get('size')[1]
-			await channel.send(f'{before.display_name} is now playing {after.activity.name}. Party size is {party_min}/{party_max}')
-
-
+			try:
+				party_min = after.activity.party['size'][0]
+				party_max = after.activity.party['size'][1]
+				print(party_min, party_max)
+				await channel.send(f'{after.display_name} is now playing {after.activity.name}. Party size is {party_min}/{party_max}')
+			except Exception:
+				await channel.send(f'{after.display_name} is now playing {after.activity.name}.')
+		
+		if before.activity != None and after.activity == None:
+			channel = before.guild.system_channel
+			await channel.send(f'{after.display_name} has finished playing {before.activity.name}.')
 
 def setup(bot):
 	bot.add_cog(Events(bot))
