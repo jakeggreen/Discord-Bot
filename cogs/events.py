0d50580d637eb403d314b9fc0de0aceb18d7dc2c
jakeggreen <80248380+jakeggreen@users.noreply.github.com>
2021-04-06 14:42:15 +0100

    added status change

--- cogs/events.py
+++ cogs/events.py
@@ -13,10 +13,23 @@ class Events(Cog):
 		pass
 
 	@Cog.listener()
-	async def change_presence(member, activity=None, status=None):
+	async def change_presence(member, activity, status):
 		activity = member.activity.name
 		status = member.status
-		print(member, activity, status)
+		print(member.display_name, activity, status)
+
+	@Cog.listener()
+	async def on_member_update(self, before, after):
+		if before.activity == None:
+			channel = before.guild.system_channel
+			await channel.send(f'{before.display_name} is now {after.status}!')
+		else:
+			channel = before.guild.system_channel
+			party_min = after.activity.party.get('size')[0]
+			party_max = after.activity.party.get('size')[1]
+			await channel.send(f'{before.display_name} is now playing {after.activity.name}. Party size is {party_min}/{party_max}')
+
+
 
 def setup(bot):
 	bot.add_cog(Events(bot))
