bdac4e5ac0cfcb6e001b67d97716ee22d65354a6
jakeggreen <80248380+jakeggreen@users.noreply.github.com>
2021-04-09 16:20:12 +0100

    log

--- lib/db/build.sql
+++ lib/db/build.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS tbl_commands_log (
-user varchar(max) NULL,
-command varchar(max) NULL, 
+user varchar NULL,
+command varchar NULL, 
 command_datetime datetime NULL
 );
 
 CREATE TABLE IF NOT EXISTS tbl_errors_log (
 error_id int NOT NULL,
-error_name varchar(max) NULL, 
-error_message varchar(max) NULL, 
+error_name varchar NULL, 
+error_message varchar NULL, 
 error_datetime datetime NULL,
-error_exception varchar(255) NULL
+error_exception varchar NULL
 );
