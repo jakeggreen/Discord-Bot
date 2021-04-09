53ace6a90be78b16cd593a55ea513d983888364d
Peter Cassell <peterca@ebasetech.com>
2021-04-09 16:13:41 +0100

    wording change

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ import pytz
 
 class Bot_Settings(object):
 	def __init__(self):
-		print('Initialising bot settings')
 		self.PREFIX = '.'
 		self.tz = pytz.timezone('Europe/London')
 		self.date_f1 = '%d-%m-%Y %H:%M:%S'
+		print('Bot Settings Initialised.')
